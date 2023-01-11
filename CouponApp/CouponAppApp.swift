1d1ce7f4d23051d2573ce1c5e9fc13a6869313e4
Hoa Truong <hoatruong@Hoas-Mac-mini.local>
2023-01-10 23:19:16 -0600

    first commit

---
+++ CouponApp/CouponAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouponAppApp.swift
+//  CouponApp
+//
+//  Created by Hoa Truong on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CouponAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
